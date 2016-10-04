a7f09df3921b41814c15352ba9f489cb6318f149
Poag <Poag@users.noreply.github.com>
2016-10-04 08:36:33 +0100

    Update 40_nadekoinstall.sh

--- init/40_nadekoinstall.sh
+++ init/40_nadekoinstall.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 ##Install NadekoBot
-#Cleanup Old
-rm -rf /root/NadekoBot
 
 #Get NadekoBot Version
-git clone -q --branch=1.0 https://github.com/Kwoth/NadekoBot.git /root/NadekoBot
+git clone --branch=1.0 https://github.com/Kwoth/NadekoBot.git /NadekoBot
 git checkout -qf 0df0eea6c0e1c451f0ebf4dffbb50a3ecd99eda5
 git submodule update --init -recursive
 dotnet restore
 
 #Build NadekoBot
-cd /root/NadekoBot
+cd /NadekoBot
 dotnet build
 
-#Sync any new data files
-rsync --ignore-existing -r /root/NadekoBot/data /config
-
 #Remove data folder
-rm -rf /root/NadekoBot/data
+rm -rf /NadekoBot/data
