e56a8bf555e0b595f7cd40b71abe956daed7ff4e
Poag <Poag@users.noreply.github.com>
2016-10-04 09:21:59 +0100

    Update 50_configuration.sh

--- init/50_configuration.sh
+++ init/50_configuration.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Test Configuration
-#if test -f "/config/credentials.json"; then echo "Creds Exist"; else cp /root/NadekoBot/credentials_example.json /config/credentials.json; fi
+#if test -f "/config/credentials.json"; then echo "Creds Exist"; else cp /NadekoBot/credentials_example.json /config/credentials.json; fi
 
 #Remove Built in
-#rm /root/NadekoBot/credentials.json
+#rm /NadekoBot/credentials.json
 
 #Adding Config file sym links
-#ln -s /config/credentials.json /root/NadekoBot/credentials.json
-#ln -s /config/data /root/NadekoBot/data
+#ln -s /config/credentials.json /NadekoBot/credentials.json
+#ln -s /config/data /NadekoBot/data
