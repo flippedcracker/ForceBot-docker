ce4e00dbf77a6896ab14afe3d5eab845674d7211
Poag <Poag@users.noreply.github.com>
2016-10-05 09:11:05 +0100

    Update 40_nadekoinstall.sh

--- init/40_nadekoinstall.sh
+++ init/40_nadekoinstall.sh
@@ -4,14 +4,13 @@
 rm -rf /root/NadekoBot
 
 #Get NadekoBot Version
-echo Cloning
-git clone --branch=1.0 https://github.com/Kwoth/NadekoBot.git 
+git clone --branch=1.0 https://github.com/Kwoth/NadekoBot.git /root/NadekoBot
 cd NadekoBot
-echo Checkout
+echo Checking Out
 git checkout -qf 0df0eea6c0e1c451f0ebf4dffbb50a3ecd99eda5
-echo Submodule
 git submodule update --init
 echo Dotnet Restore
+cd src/NadekoBot
 dotnet restore
 
 #Build NadekoBot
