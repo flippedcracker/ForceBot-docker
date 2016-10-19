837cd71e9375379e24d9abdc61eefe057f77f3c6
Robert Mills <robert@flippedcracker.net>
2016-10-19 15:07:43 -0700

    Update 40_nadekoinstall.sh

--- init/40_nadekoinstall.sh
+++ init/40_nadekoinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##Install NadekoBot
 #Cleanup Old
-rm -rf /root/NadekoBot
+rm -rf /mnt/user/appdata/forcebot/NadekoBot
 
 #Get NadekoBot Version
 if [[ "$VERSION" = dev ]]; then
@@ -10,10 +10,10 @@ else
         curl -s https://api.github.com/repos/flippedcracker/ForceBot/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip
 fi
 
-unzip /root/NadekoBot.latest.zip -d /root/NadekoBot
+unzip /mnt/user/appdata/forcebot/NadekoBot.latest.zip -d /mnt/user/appdata/forcebot/NadekoBot
 
 #Sync any new data files
-rsync --ignore-existing -r /root/NadekoBot/data /config
+rsync --ignore-existing -r /mnt/user/appdata/forcebot/NadekoBot/data /config
 
 #Remove data folder
-rm -rf /root/NadekoBot/data
+rm -rf /mnt/user/appdata/forcebot/NadekoBot/data
