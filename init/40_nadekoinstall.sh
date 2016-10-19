9780c59623f3ad5bf6f8e497f8d42ad5902c5cf7
Robert Mills <robert@flippedcracker.net>
2016-10-19 14:41:41 -0700

    Update 40_nadekoinstall.sh

--- init/40_nadekoinstall.sh
+++ init/40_nadekoinstall.sh
@@ -5,9 +5,9 @@ rm -rf /root/NadekoBot
 
 #Get NadekoBot Version
 if [[ "$VERSION" = dev ]]; then
-        curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip
+        curl -s https://api.github.com/repos/flippedcracker/ForceBot/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip
 else
-        curl -s https://api.github.com/repos/Kwoth/NadekoBot/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip
+        curl -s https://api.github.com/repos/flippedcracker/ForceBot/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4|wget -i - -O /root/NadekoBot.latest.zip
 fi
 
 unzip /root/NadekoBot.latest.zip -d /root/NadekoBot
