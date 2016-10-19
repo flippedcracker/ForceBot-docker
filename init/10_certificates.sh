ba01a2f89da552ea0eb8033ba37caf58ae39ddd7
Robert Mills <robert@flippedcracker.net>
2016-10-19 15:22:31 -0700

    Update 10_certificates.sh

--- init/10_certificates.sh
+++ init/10_certificates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Import Certificates
-mozroots --import --ask-remove --machine
+cert-sync --import --ask-remove --machine
 echo Y |certmgr --ssl https://gateway.discord.gg /all
 echo Y |certmgr --ssl https://gateway.discord.gg /all
 echo Y |certmgr --ssl https://gateway.discord.gg /all
